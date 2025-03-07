ec0cdae544e459505b5bbc856bfadfa6ef2933b5
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2025-03-08 00:39:54 +0400

    update

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -18,12 +18,37 @@ private:
 			insert(node->_right, value);
 		}
 	}
+	Node* copy(Node*& node) {
+		if (node == nullptr) {
+			return nullptr;
+		}
+		Node* newNode = new Node(node->_info);
+		newNode->_left = copy(node->_left);
+		newNode->_right = copy(node->_right);
+	}
+	void popNode(Node*& node) {
+		if (node == nullptr) {
+			popNode(node->_left);
+			popNode(node->_left);
+			delete node;
+		}
+	}
 public:
 	BinaryTree() :_root(nullptr) {}
 	BinaryTree(const BinaryTree& other) {
-		//AAAAAAAAAAAA
+		copy(other._root);
+		popNode(other._root);
 	}
-
+	~BinaryTree() {
+		popNode(_root);
+	}
+	BinaryTree operator=(const BinaryTree& other) {
+		if (this != other) {
+			popNode(_root);
+			_root = copy(other._root);
+		}
+		return *this;
+	}
 
 
 };
